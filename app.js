e02302b9776cf8a3837ab3ddc4f4d0b16a2a9fa8
kevinjacob2001 <kevinjacob2001@gmail.com>
2020-08-26 21:33:52 +0530

    added middleware

--- app.js
+++ app.js
@@ -10,8 +10,14 @@ const app=express();
 mongoose.connect(url,{useNewUrlParser:true})
 const con=mongoose.connection
 
+const alienRouter=require("./routers/aliens");
+app.use("/aliens",alienRouter);
+
+
 con.on("open",()=>{
     console.log("Connected...")
 })
 
-app.listen(9000)
+app.listen(9000,()=>{
+    console.log("Server started listening at port:9000")
+})
