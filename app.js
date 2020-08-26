435f3f86448a98bf6e8634f5e83ecc3d2ef60f5c
kevinjacob2001 <kevinjacob2001@gmail.com>
2020-08-26 22:28:17 +0530

    Added one another middleware and completed adding the POST Request

--- app.js
+++ app.js
@@ -4,19 +4,18 @@ const url="mongodb://localhost/AlienDBex"
 
 const app=express();
 
-
-
-
 mongoose.connect(url,{useNewUrlParser:true})
 const con=mongoose.connection
-
-const alienRouter=require("./routes/aliens");
-app.use("/aliens",alienRouter);
-
 
 con.on("open",()=>{
     console.log("Connected...")
 })
+
+app.use(express.json())
+
+const alienRouter=require("./routes/aliens");
+app.use("/aliens",alienRouter);
+
 
 app.listen(9000,()=>{
     console.log("Server started listening at port:9000")
--- routes/aliens.js
+++ routes/aliens.js
@@ -13,14 +13,16 @@ router.get("/",async(req,res)=>{
     }
 })
 
-router.post("/",async(req,res){
+
+router.post("/",async(req,res)=>{
     const alien=new Alien({
         name:req.body.name,
         tech:req.body.tech,
         sub:req.body.sub
     })
 try{
-
+const a1=await alien.save();
+res.json(a1);
 }
 
 catch(err){
